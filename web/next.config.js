51e19f6754c817a304b2f2d626f96d5605e45de7
Son <lutcbs@gmail.com>
2026-05-12 00:16:26 +0700

    fix: include content dir in Vercel deployment

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['fs-extra', 'pdf-parse'],
+  outputFileTracingRoot: path.join(__dirname, '../'),
 };
 
 module.exports = nextConfig;
